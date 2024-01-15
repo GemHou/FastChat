b60fce5db2d4c5fec9443b2a284117b1730c626d
gemhou159 <784210432@qq.com>
2024-01-15 17:14:32 +0800

    hj: mentioned  sentence

--- fastchat/serve/hj_infer_keyword.py
+++ fastchat/serve/hj_infer_keyword.py
@@ -16,7 +16,7 @@ def get_list_keywords():
     list_list_keywords = [['团战世界', '游戏规则', '团队协同', '角色能力', '敌人击败', '治疗', '输出角色', '大量伤害', '防御', '承伤职业', '仇恨', 'BOSS战', '基础属性', '伤害计算', '技能释放', 'CD', '冷却时间', '单体伤害', '范围伤害', '单体恢复', '范围恢复', '敌对单位', '攻击优先级', '仇恨列表', 'BOSS攻击对象', '增加仇恨', '减少仇恨', '仇恨控制', '伤害预测', 'BOSS伤害对象', '仇恨累积速度', '普通伤害技能', '治疗技能', '高仇恨技能', 'BOSS攻击保护', '无法承受BOSS攻击'],
         ['团战世界', '游戏规则', '游戏胜负判定', '杂兵战', 'BOSS战', '我方人员全部被消灭', '战斗时间', '敌人强化', 'BOSS狂暴化', '生存', '高效输出', '职业倾向', '坦克倾向', '输出倾向', '治疗倾向', '辅助倾向', '角色特性', '防御类属性', '仇恨技能', '自保技能', '破甲', '嘲讽', '盾墙', '火球', '炎爆', '火焰冲击', '小恢复术', '大恢复术', '生命回流', '圣盾术', '硬化术', '回春图腾', '脆弱术', '战斗流程', '仇恨控制', 'CD管理', '输出优化', '治疗策略', '辅助技能'],
         ['团战世界', '游戏规则', '减伤', 'BUFF', '伤害减少', '治疗职业', '阵亡概率', '指挥', '大招', '坦克开减伤', 'AOE', '群体减伤', 'Rush', '固定流程', '切换目标', '敌人掉血', 'AOE技能', 'T', '主T', '副T', '吸引仇恨', '控制仇恨', '吃药', '生命值提升', 'OT', '仇恨失控', '嘲讽技能', '团灭', 'T拉回来', '奶妈抬血', '点名', '压一波血', '灌伤害']]
-    print("keywords_list: ", list_list_keywords)
+    # print("keywords_list: ", list_list_keywords)
 
     list_keywords = merge_and_deduplicate_keywords(list_list_keywords)
     return list_keywords
@@ -29,16 +29,23 @@ def get_list_corpus():
     with open(input_file_name, 'r', encoding='utf-8') as file:
         str_full_corpus = file.read()  # 读取文件的全部内容
         # print("str_full_corpus: ", str_full_corpus)
-    print("str_full_corpus: ", str_full_corpus)
+    # print("str_full_corpus: ", str_full_corpus)
     list_corpus = split_text_by_dot_and_semicolon(str_full_corpus)
     return list_corpus
 
 def main():
     list_keywords = get_list_keywords()
-    print("list_keywords: ", list_keywords)
+    # print("list_keywords: ", list_keywords)
 
     list_corpus = get_list_corpus()
-    print("list_corpus: ", list_corpus)
+    # print("list_corpus: ", list_corpus)
+
+    for keyword in list_keywords:
+        print("keyword: ", keyword)
+        for sentence in list_corpus:
+            if keyword in sentence:
+                print("sentence: ", sentence)
+
 
 if __name__ == "__main__":
     main()
