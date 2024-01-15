77b9646a227488e664caf582423a0be1dc22c48f
gemhou159 <784210432@qq.com>
2024-01-15 21:07:15 +0800

    hj: 请围绕关键词'" + list_valid_keywords_sentences[i][0]

--- fastchat/serve/hj_infer_keyword.py
+++ fastchat/serve/hj_infer_keyword.py
@@ -22,7 +22,8 @@ def merge_and_deduplicate_keywords(list_list_keywords):
     return list_keywords
 
 def get_list_keywords():
-    list_list_keywords = [['团战世界', '游戏规则', '团队协同', '角色能力', '敌人击败', '治疗', '输出角色', '大量伤害', '防御', '承伤职业', '仇恨', 'BOSS战', '基础属性', '伤害计算', '技能释放', 'CD', '冷却时间', '单体伤害', '范围伤害', '单体恢复', '范围恢复', '敌对单位', '攻击优先级', '仇恨列表', 'BOSS攻击对象', '增加仇恨', '减少仇恨', '仇恨控制', '伤害预测', 'BOSS伤害对象', '仇恨累积速度', '普通伤害技能', '治疗技能', '高仇恨技能', 'BOSS攻击保护', '无法承受BOSS攻击'],
+    list_list_keywords = [['火球', '大治疗术', '硬化', '圣盾', '破甲', '嘲讽', '炎爆'], 
+        ['团战世界', '游戏规则', '团队协同', '角色能力', '敌人击败', '治疗', '输出角色', '大量伤害', '防御', '承伤职业', '仇恨', 'BOSS战', '基础属性', '伤害计算', '技能释放', 'CD', '冷却时间', '单体伤害', '范围伤害', '单体恢复', '范围恢复', '敌对单位', '攻击优先级', '仇恨列表', 'BOSS攻击对象', '增加仇恨', '减少仇恨', '仇恨控制', '伤害预测', 'BOSS伤害对象', '仇恨累积速度', '普通伤害技能', '治疗技能', '高仇恨技能', 'BOSS攻击保护', '无法承受BOSS攻击'],
         ['团战世界', '游戏规则', '游戏胜负判定', '杂兵战', 'BOSS战', '我方人员全部被消灭', '战斗时间', '敌人强化', 'BOSS狂暴化', '生存', '高效输出', '职业倾向', '坦克倾向', '输出倾向', '治疗倾向', '辅助倾向', '角色特性', '防御类属性', '仇恨技能', '自保技能', '破甲', '嘲讽', '盾墙', '火球', '炎爆', '火焰冲击', '小恢复术', '大恢复术', '生命回流', '圣盾术', '硬化术', '回春图腾', '脆弱术', '战斗流程', '仇恨控制', 'CD管理', '输出优化', '治疗策略', '辅助技能'],
         ['团战世界', '游戏规则', '减伤', 'BUFF', '伤害减少', '治疗职业', '阵亡概率', '指挥', '大招', '坦克开减伤', 'AOE', '群体减伤', 'Rush', '固定流程', '切换目标', '敌人掉血', 'AOE技能', 'T', '主T', '副T', '吸引仇恨', '控制仇恨', '吃药', '生命值提升', 'OT', '仇恨失控', '嘲讽技能', '团灭', 'T拉回来', '奶妈抬血', '点名', '压一波血', '灌伤害']]
     # print("keywords_list: ", list_list_keywords)
@@ -62,17 +63,7 @@ def new_chat(model_path):
         conv = get_conversation_template(model_path)
         return conv
 
-
-def main():
-    list_keywords = get_list_keywords()
-    # print("list_keywords: ", list_keywords)
-
-    list_corpus = get_list_corpus()
-    # print("list_corpus: ", list_corpus)
-
-    list_valid_keywords_sentences = get_list_valid_keywords_sentences(list_keywords, list_corpus)
-    # print("list_valid_keywords_sentences: ", list_valid_keywords_sentences)
-
+def load_llm_model():
     model_path = MODEL_PATH
     device = "cuda"
     num_gpus = 1
@@ -123,9 +114,23 @@ def main():
     max_new_tokens = 2048
     context_len = get_context_length(model.config)
     judge_sent_end = False
-    str_prompt_list = ["hello", "what is your name?", "goodbye"]
-    str_prompt = "hello"
-    for str_prompt in str_prompt_list:
+    return model_path,device,model,tokenizer,generate_stream_func,temperature,repetition_penalty,max_new_tokens,context_len,judge_sent_end
+
+
+def main():
+    list_keywords = get_list_keywords()
+    # print("list_keywords: ", list_keywords)
+
+    list_corpus = get_list_corpus()
+    # print("list_corpus: ", list_corpus)
+
+    list_valid_keywords_sentences = get_list_valid_keywords_sentences(list_keywords, list_corpus)
+    # print("list_valid_keywords_sentences: ", list_valid_keywords_sentences)
+
+    model_path, device, model, tokenizer, generate_stream_func, temperature, repetition_penalty, max_new_tokens, context_len, judge_sent_end = load_llm_model()
+    for i in range(10):
+        str_prompt = "基于以下语料，请围绕关键词'" + list_valid_keywords_sentences[i][0] + "'尝试生成1个简洁精简的问题和回答，整理成问答格式，不要胡编乱造内容。语料：" + list_valid_keywords_sentences[i][1][0]
+        print("str_prompt: ", str_prompt)
         conv = new_chat(model_path)
         conv.append_message(conv.roles[0], str_prompt)
         conv.append_message(conv.roles[1], None)
@@ -155,7 +160,6 @@ def main():
         duration = time.time() - t
 
         print("duration: ", duration)
-        # print("outputs: ", outputs)
 
 
 if __name__ == "__main__":
