f31264e6bcc1c22500a7b3c4024756e16275039e
gemhou159 <784210432@qq.com>
2024-01-26 20:25:01 +0800

    hj: ppo

---
+++ fastchat/train/hj_ppo.py
@@ -0,0 +1,40 @@
+import torch
+from trl import PPOTrainer, PPOConfig
+from trl import AutoModelForCausalLMWithValueHead
+
+from fastchat.serve.hj_utils_llm import load_llm_model
+
+def main():
+    model_name_or_path = "/mnt/nfs/zhangqi/zhangqi_nfs/DLM-project/public_models/modelWeights/vicuna-13b-v1.5"
+    device = "cuda"
+    model, tokenizer = load_llm_model(model_path=model_name_or_path, device=device)
+
+    model: "AutoModelForCausalLMWithValueHead" = AutoModelForCausalLMWithValueHead.from_pretrained(model)
+
+    ppo_config = PPOConfig(mini_batch_size=2,
+                           batch_size=2,
+                           )
+    ppo_trainer = PPOTrainer(config=ppo_config,
+                             model=model,
+                             tokenizer=tokenizer,                             
+                             )
+    str_queries = "who are you"
+    batchEncoding_queries = tokenizer(str_queries, return_tensors="pt")
+    tensor_token_queries = batchEncoding_queries["input_ids"]  # [1, 4]
+
+    str_responses = "JingHou"
+    batchEncoding_responses = tokenizer(str_responses, return_tensors="pt")
+    tensor_token_responses = batchEncoding_responses["input_ids"]  # [1, 5]
+
+    int_reward = 10
+    tensor_value = torch.tensor(int_reward, dtype=torch.int32)
+    
+    stats = ppo_trainer.step([tensor_token_queries, tensor_token_queries], 
+                             [tensor_token_responses, tensor_token_responses], 
+                             [tensor_value, tensor_value])
+
+    print("Finished...")
+
+
+if __name__ == "__main__":
+    main()
