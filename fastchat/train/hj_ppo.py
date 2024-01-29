e4609a2f216a213c4f834741dd17e1fafb53d548
gemhou159 <784210432@qq.com>
2024-01-29 19:32:48 +0800

    hj: refactor

--- fastchat/train/hj_ppo.py
+++ fastchat/train/hj_ppo.py
@@ -20,12 +20,10 @@ class LoraArguments:
     lora_bias: str = "none"
     q_lora: bool = False
 
-
-def main():
+def load_trainer():
     model_name_or_path = "/mnt/nfs/zhangqi/zhangqi_nfs/DLM-project/public_models/modelWeights/vicuna-7b-v1.5"  # 13b 7b
     device = "cuda"  # cuda cpu
     model, tokenizer = load_llm_model(model_path=model_name_or_path, device=device)  # -> transformers
-    # tokenizer.model_max_length = 10
     model.to(torch.bfloat16)
 
     lora_args = LoraArguments(lora_r=2)
@@ -36,8 +34,6 @@ def main():
         lora_dropout=lora_args.lora_dropout,
         bias=lora_args.lora_bias,
         task_type="CAUSAL_LM",
-        # target_modules=[name.strip() for name in lora_target.split(",")],
-        # init_lora_weights="loftq",
         inference_mode=False,
     )
     model = get_peft_model(model, lora_config)  # transformers -> peft
@@ -52,25 +48,28 @@ def main():
                              tokenizer=tokenizer,
                              device=device
                              )
+                             
+    return tokenizer,ppo_trainer
+
+def change_data_format(tokenizer, str_queries, str_responses, float_reward):
+    batchEncoding_queries = tokenizer(str_queries, return_tensors="pt", truncation=True,)
+    tensor_token_queries = batchEncoding_queries["input_ids"]
+    tensor_token_queries = tensor_token_queries[0]
+    batchEncoding_responses = tokenizer(str_responses, return_tensors="pt", truncation=True,)
+    tensor_token_responses = batchEncoding_responses["input_ids"]
+    tensor_token_responses = tensor_token_responses[0]
+    tensor_value = torch.tensor(float_reward, dtype=torch.float32)
+    return tensor_token_queries,tensor_token_responses,tensor_value
+
+
+def main():
+    tokenizer, ppo_trainer = load_trainer()
 
     str_queries = "who are you"
-    batchEncoding_queries = tokenizer(str_queries, return_tensors="pt",
-        # padding="max_length",
-        # max_length=tokenizer.model_max_length,
-        truncation=True,)
-    tensor_token_queries = batchEncoding_queries["input_ids"]  # [1, 4096]
-    tensor_token_queries = tensor_token_queries[0]
-
     str_responses = "JingHou"
-    batchEncoding_responses = tokenizer(str_responses, return_tensors="pt",
-        # padding="max_length",
-        # max_length=tokenizer.model_max_length,
-        truncation=True,)
-    tensor_token_responses = batchEncoding_responses["input_ids"]  # [1, 4096]
-    tensor_token_responses = tensor_token_responses[0]
+    float_reward = 10.01
 
-    int_reward = 10.01
-    tensor_value = torch.tensor(int_reward, dtype=torch.float32)
+    tensor_token_queries, tensor_token_responses, tensor_value = change_data_format(tokenizer, str_queries, str_responses, float_reward)
     
     stats = ppo_trainer.step([tensor_token_queries], 
                              [tensor_token_responses], 
